0a2b3852cbeea31be9260577350c22cfcb2f552e
Rawphel <rawphel@outlook.com>
2022-02-14 18:44:18 +0800

    Fix rights migration file again

--- db/migrate/20220214103019_add_default_value_to_users.rb
+++ db/migrate/20220214103019_add_default_value_to_users.rb
@@ -1,9 +1,9 @@
 class AddDefaultValueToUsers < ActiveRecord::Migration[6.1]
   def up
-    change_column_default :profiles, :show_attribute, true
+    change_column_default :users, :rights, true
   end
 
   def down
-    change_column_default :profiles, :show_attribute, nil
+    change_column_default :users, :rights, nil
   end
 end
